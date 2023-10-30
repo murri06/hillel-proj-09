cce58e00f899372cadeddce844e0308814bd8767
murri06 <ruslanliakhovets@outlook.com>
2023-10-30 10:49:00 +0200

    added echo lines for clear understanding

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
-
-
 $encoder = [new JsonEncoder()];
 $normalizers = [new ObjectNormalizer()];
 $serializer = new Serializer($normalizers, $encoder);
@@ -16,9 +14,13 @@ $shuttle = new SpaceShuttle("Earth", 27, 2572.5);
 
 $jsonSerialised = $serializer->serialize($shuttle, "json");
 
-echo $jsonSerialised . PHP_EOL;
+echo "Created object:" . PHP_EOL;
+var_dump($shuttle);
+
+echo "JSON serialized object:" . $jsonSerialised . PHP_EOL;
 
 $shuttle2 = $serializer->deserialize($jsonSerialised, SpaceShuttle::class, 'json');
+echo "Deserialized object as a new instance:" . PHP_EOL;
 var_dump($shuttle2);
 
 echo $shuttle2->getHomePlanet() . PHP_EOL;
